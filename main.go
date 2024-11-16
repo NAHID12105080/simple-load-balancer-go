5fff3ef55a815b8eae31686d605a35fc2d422a1e
NAHID12105080 <nahid151341@gmail.com>
2024-11-16 10:07:03 +0600

    resolved all the errors

--- main.go
+++ main.go
@@ -1,102 +1,103 @@
 package main
 
 import (
-	
 	"fmt"
 	"net/http"
 	"net/http/httputil"
-
+	"net/url"
 )
+
 type Server interface {
 	Address() string
 	isAlive() bool
 	Serve(res http.ResponseWriter, req *http.Request)
-	
 }
 
 type simpleServer struct {
 	address string
-	proxy *httputil.ReverseProxy
-
+	proxy   *httputil.ReverseProxy
 }
 
 type loadBalancer struct {
-	port string
-	roundRobinIndex int
-	servers []*simpleServer
+	port             string
+	roundRobinIndex  int
+	servers          []Server
 }
 
-func NewLoadBalancer(port string) *loadBalancer{
+func NewLoadBalancer(port string, servers []Server) *loadBalancer {
 	return &loadBalancer{
-		port: port,
+		port:            port,
 		roundRobinIndex: 0,
-		servers: make([]*simpleServer,0),
+		servers:         servers,
 	}
 }
 
-func (lb *loadBalancer) AddServer(server *simpleServer){}
+func (lb *loadBalancer) AddServer(server Server) {
+	lb.servers = append(lb.servers, server)
+}
 
-func (lb *loadBalancer) getNextAvailableServer() Server{
-	server:= lb.servers[lb.roundRobinIndex]
-	for !server.isAlive() {
+func (lb *loadBalancer) getNextAvailableServer() Server {
+	if len(lb.servers) == 0 {
+		panic("No servers available")
+	}
+
+	for {
+		server := lb.servers[lb.roundRobinIndex%len(lb.servers)]
 		lb.roundRobinIndex++
-		server=lb.servers[lb.roundRobinCount%len(lb.servers)]
+		if server.isAlive() {
+			return server
+		}
 	}
-	lb.roundRobinIndex++
-	return server
-
-
 }
 
-func (lb *loadBalancer) serverProxy(rw http.ResponseWriter, req *http.Request){
-	targetServer:= lb.getNextAvailableServer()
-	fmt.Println("Proxying request to server: ", targetServer.address)
-	targetServer.Serve(rw,req) 
+func (lb *loadBalancer) serverProxy(rw http.ResponseWriter, req *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Println("Proxying request to server:", targetServer.Address())
+	targetServer.Serve(rw, req)
 }
 
-func handleError(err error){
-	if err != nil{
+func handleError(err error) {
+	if err != nil {
 		panic(err)
-		
 	}
 }
 
-func newSimpleServer(address string ) *simpleServer{
-	serverUrl,err= url.Parse(address)
+func newSimpleServer(address string) *simpleServer {
+	serverUrl, err := url.Parse(address)
 	handleError(err)
 	return &simpleServer{
 		address: address,
-		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+		proxy:   httputil.NewSingleHostReverseProxy(serverUrl),
 	}
 }
 
-func Address(s *simpleServer) string{
+func (s *simpleServer) Address() string {
 	return s.address
 }
 
 func (s *simpleServer) isAlive() bool {
+	// Always return true in this example. Add health checks as needed.
 	return true
 }
 
-func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request){
-	s.proxy.ServeHTTP(rw,req)
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
 }
 
-func main(){
-	servers:= []Server{
+func main() {
+	servers := []Server{
 		newSimpleServer("https://www.facebook.com"),
 		newSimpleServer("https://www.google.com"),
 		newSimpleServer("https://www.youtube.com"),
-
 	}
 
-	lb:= NewLoadBalancer("8080",servers)
-	handleRedirect:= func(rw http.ResponseWriter, req *http.Request){
-		lb.serverProxy(rw,req)
+	lb := NewLoadBalancer("8080", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serverProxy(rw, req)
 	}
-	http.HandleFunc("/",handleRedirect)
-	fmt.println("Load Balancer is running on port 8080")
-	http.ListenAndServe(":8080",nil)
 
-	
+	http.HandleFunc("/", handleRedirect)
+	fmt.Println("Load Balancer is running on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	handleError(err)
 }
